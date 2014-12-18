3342ff7b90eec5317743b32855c11f7ccd7e1bce
Ptolemy <ptolemybarnes@gmail.com>
2014-12-18 14:04:46 +0000

    Game can validate a build plan, checking whether the game rules allow a ship of the length proposed.

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,11 @@
 require 'board'
 
 class Game
+  attr_reader :board, :rules
 
-  attr_reader :board
-  def initialize
+  def initialize(rules)
     @player = []
+    @rules  = rules
   end
 
   def register_player(player)
@@ -15,41 +16,11 @@ class Game
     @player.count
   end
 
-  def set_board_size(size)
-    @board = Board.new(size)
-  end
-
   def set_ships_prompt
      'You are ready to place the ships.'
   end
 
-  def set_battleship(head_yx, tail_yx)
-    @board.place_ship(head: head_yx, tail: tail_yx)
+  def validate_build_plan build_plan
+    (@rules[build_plan.size]) && (@rules[build_plan.size] > 0)
   end
-
-  def set_second_ship(head_yx, tail_yx)
-    @board.place_ship(head: head_yx, tail: tail_yx)
-  end
-
-  def set_fleet
-    set_ships_prompt
-    # set_battleship(head_yx, tail_yx)
-    player_input = get_player_input
-    set_battleship(player_input[0], player_input[1])
-  end
-
-  # def get_player_input
-  #   'Enter the x coordinate of head'
-  #   hx = gets.chomp.to_i
-  #   puts 'Enter the y coordinate of head'
-  #   hy = gets.chomp.to_i
-  #   puts 'Enter the x coordinate of tail'
-  #   tx = gets.chomp.to_i
-  #   puts 'Enter the y coordinate of tail'
-  #   ty = gets.chomp.to_i
-  #   head_location = [hy, hx]
-  #   tail_location = [ty, tx]
-  #   ship_location = [head_location, tail_location]
-  # end
-
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,35 +1,37 @@
 require './lib/game'
 require './lib/board'
 
+rules_array = [
+  nil, #0
+  nil, #1
+  3, #2
+  3, #3
+  2, #4
+  1, #5
+]
+
+build_plan = [[0,1],[0,2],[0,3],[0,4]]
+
 describe Game do 
-  let(:game) {Game.new}
-  let(:player1)  {double(:player)}
+  let(:rules)    { double(:rules, get_rules: rules_array)}
+  let(:game)     { Game.new(rules.get_rules)}
+  let(:player1)  { double(:player)}
+
   it 'Can register a player' do
     game.register_player(player1)
     expect(game.player_count).to eq 1
-  end
-
-  context 'Can set the board' do
-    it 'can set to any size' do
-      game.set_board_size(5)
-      expect(game.board.size).to eq 5
-    end
-
   end
 
   context 'Players can set their ships' do
     it 'will prompt player to set ships' do
       expect(game.set_ships_prompt).to eq 'You are ready to place the ships.'
     end
-
-    it 'can set the battleship' do
-      game.set_board_size(5)
-      game.set_battleship([2,1], [2,5])
-      expect(game.board.count_ship).to eq 1
+    it 'can validate a build plan against its inventory' do
+      expect(game.validate_build_plan(build_plan)).to eq(true)
     end
-
-    # it 'player should be able to input ship location' do
-    #   expect(game.get_player_input).to eq([[2,2],[5,2]])
-    # end
+    it 'returns an error when player tries to build a ship of an unavailable size' do
+      rules_array[4] = 0
+      expect(game.validate_build_plan(build_plan)).to eq(false)
+    end
   end
 end
